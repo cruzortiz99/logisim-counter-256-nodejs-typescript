57d21b1a51d95c2e9cbcd3379e7992341a81625a
Cruz Ortiz <cortiz99>
2019-05-28 23:05:08 -0400

    writing the files correctly

--- counter.ts
+++ counter.ts
@@ -1,13 +1,55 @@
 import fs from 'fs'
 import path from 'path'
-const binaryRoute = path.join(__dirname, 'counter-256.bin')
-const binaryFile = fs.createWriteStream(binaryRoute)
 
-const counter = (module: number) => {
-  let i = 0
-  for (i = 0; i < module; i++) {
-    binaryFile.write(`${i} `, 'binary')
-  }
+const filePath = path.join(__dirname, 'counter-256.txt')
+const file = fs.createWriteStream(filePath)
+
+type CounterWriter = (count: number, file: fs.WriteStream) => void
+type Counter = (module: number, file: fs.WriteStream) => void
+
+const writeBinary: CounterWriter = (count: number, file: fs.WriteStream) => {
+  file.write(`${count}`)
+}
+const writeBCD: CounterWriter = (count: number, file: fs.WriteStream) => {
+  file.write(`${count}`)
 }
 
-counter(256)
+const writeCountIn = (
+  count: number,
+  encoding: 'bcd' | 'binary',
+  file: fs.WriteStream
+) => {
+  switch (encoding) {
+    case 'bcd':
+      writeBCD(count, file)
+      break
+    case 'binary':
+      writeBinary(count, file)
+      break
+  }
+  file.write(' ')
+}
+
+const counterAsc: Counter = (module, file) => {
+  let coded: ['binary', 'bcd'] = ['binary', 'bcd']
+  coded.forEach((code) => {
+    let i = 0
+    for (i; i < module; i++) {
+      writeCountIn(i, code, file)
+    }
+    file.write('\n')
+  })
+}
+const counterDes: Counter = (module, file) => {
+  let coded: ['binary', 'bcd'] = ['binary', 'bcd']
+  coded.forEach((code) => {
+    let i = module - 1
+    for (i; i >= 0; i--) {
+      writeCountIn(i, code, file)
+    }
+    file.write('\n')
+  })
+}
+
+counterAsc(10, file)
+counterDes(10, file)
